902c4458578427b77d002b1292ed31e8b031fc85
casper <wardenaar@physics.leidenuniv.nl>
2026-03-25 12:15:32 +0100

    added the src folder

---
+++ src/interactive.py
@@ -0,0 +1,34 @@
+from IPython import start_ipython
+import sys
+import casperfpga.casperfpga as casperfpga
+import os
+
+
+if len(sys.argv) > 1:
+    rp_ip = sys.argv[1]
+else:
+    quit("Usage: python interactive.py <rp_ip_address>")
+    
+# Remove custom args so IPython doesn't see them
+sys.argv = [sys.argv[0]]
+
+fpga = casperfpga.CasperFpga(rp_ip)
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+directory = os.path.join(base_dir, "..", "model_composer", "pulse_generator", "outputs")
+newest_file = max(
+    (os.path.join(directory, f) for f in os.listdir(directory)),
+    key=os.path.getmtime
+)
+
+try:
+    fpga.upload_to_ram_and_program(newest_file)
+except Exception as e:
+    print(f"Failed to upload FPGA program: {e}")
+    raise
+
+start_ipython()
+
+
+
