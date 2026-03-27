8256e0fbf3c93f66c4dd327c80db1d4da44cdc53
Flint Wardenaar <wardenaar@physics.leidenuniv.nl>
2026-03-27 13:39:41 +0100

    added basic register write function

--- src/packages/fpga_interface.py
+++ src/packages/fpga_interface.py
@@ -4,8 +4,6 @@ import struct
 
 # Base address (must be page-aligned!)
 BASE_ADDR = 0x40000000
-OFFSET = 0xC8  # your register offset
-
 PAGE_SIZE = mmap.PAGESIZE
 MAP_SIZE = PAGE_SIZE
 
@@ -25,22 +23,39 @@ class FPGAInterface:
             )
 
     # Parse the .fpg file to get register names and offsets, and store in a dictionary
-    def load_register_map(self, register_map):
-        with open(register_map, "r") as f:
-            self.register_map = {}
-            for line in f:
-                if line.strip() and line.startswith("?register"):
-                    data = line.split(" ")
-                    self.register_map[data[1].strip()] = int(data[2].strip(), 0)
-    
-    def write_int(self, offset, value):
-        if self.mem is None:
-            raise RuntimeError("Memory not mapped")
-        if offset < 0 or offset + 4 > self.map_size:
-            raise ValueError("Offset out of bounds")
+    def load_register_map(self, register_map_dir, debug=False):
+        try:
+            with open(register_map_dir, "r", errors = "ignore") as f:
+                self.register_map = {}
+                for line in f:
+                    if debug:
+                        print(line)
+                    if line.strip() and line.startswith("?register"):
+                        data = line.split("\t")
+                        self.register_map[data[1].strip()] = int(data[2].strip(), 0)
+                    elif line.strip() and line.startswith("?quit"):
+                        print("Successfully finished parsing")
+                        break
+            return True
+        except Exception as e:
+            print(f"Error loading register map: {e}")
+            return False
+            
+            
+    # def write_int(self, offset, value):
+    #     if self.mem is None:
+    #         raise RuntimeError("Memory not mapped")
+    #     if offset < 0 or offset + 4 > self.map_size:
+    #         raise ValueError("Offset out of bounds")
         
-        self.mem[offset:offset+4] = struct.pack("<I", value)
-    
+        # self.mem[offset:offset+4] = struct.pack("<I", value)
+        
+    def write_register(self, register_name, value):
+        if register_name not in self.register_map:
+            raise ValueError(f"Register {register_name} not found in register map")
+        mem_loc = self.register_map[register_name]
+        self.mem[mem_loc:mem_loc+4] = struct.pack("<I", value)
+
 
     
     
